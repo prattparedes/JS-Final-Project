bb77a9722194a26b7328258db00e14ff1f18b190
Pratt <pratt.paredes@gmail.com>
2022-09-28 12:03:36 -0500

    creating ID search

--- movies.js
+++ movies.js
@@ -1,5 +1,4 @@
 const moviesEl = document.querySelector(".movies")
-console.log(moviesEl)
 
 function searchMovie(search) {
     renderMovies(search.target.value)
@@ -8,26 +7,36 @@ function searchMovie(search) {
 async function renderMovies(search) {
     const movies = await fetch(`https://www.omdbapi.com/?s=${search}&apikey=7ca89627`)
     const moviesData = await movies.json()
-    console.log(moviesData.Search)
-    moviesEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie)).join('')
+    const moviesData6 = moviesData.Search.slice(0, 6)
+    moviesEl.innerHTML = moviesData6.map(movie => {
+        
+        movieHTML(movie)
+    }).join('')
+    console.log(moviesData6)
 }
 
-function movieHTML(movie) {
+async function imdbData(ID) {
+    const movie = await fetch(`http://www.omdbapi.com/?i=${ID}&apikey=7ca89627`)
+    const movieData = await movie.json()
+    return movieData
+}
+
+async function movieHTML(movieID) {
+    const ID = movieID.imdbID
+    const movieDataID = await imdbData(ID)
     return `<div class="movie">
     <figure class="movie__img--wrapper">
-        <img src="${movie.Poster}" alt="" class="movie__img">
+        <img src="${movieDataID.Poster}" alt="" class="movie__img">
     </figure>
     <div class="movie__description">
-        <h3 class="movie__title">${movie.Title}</h3>
-        <h4 class="movie__release">${movie.Year}</h4>
-        <p class="movie__genres">Action, Adventure, Comedy</p>
-        <p class="movie__director">James Gunn</p>
+        <h3 class="movie__title">${movieDataID.Title}</h3>
+        <h4 class="movie__release">${movieDataID.Year}</h4>
+        <p class="movie__genres">${movieDataID.Genre}</p>
+        <p class="movie__director">${movieDataID.Director}</p>
         <div class="movie__synopsis">
-            The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.
+            ${movieDataID.Plot}
         </div>
-        <p class="rating">7.8 <i class="fas fa-star"></i></p>
+        <p class="rating">${movieDataID.imdbRating} <i class="fas fa-star"></i></p>
     </div>
     </div>`
 }
-
-renderMovies('Fast')
