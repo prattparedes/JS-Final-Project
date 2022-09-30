30c30495919f8debf72860b4e6cfa2f07af5a0c4
Pratt <pratt.paredes@gmail.com>
2022-09-29 23:41:31 -0500

    almost done

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function SearchMovieIndex(event) {
-    localStorage.setItem("search", event.target.value);
-    window.location.href = `${window.location.origin}/Final%20Project/movies.html` //this REROUTES to the same url of the page
+    const animationEl = document.querySelector('.header__moving-animation--wrapper')
+    animationEl.innerHTML = 
+    `<figure>
+    <img src="./Assets/MovieTape.svg" alt="" class="movietape--animation">
+    </figure>`
+    
+    animationEl.classList += ' moving'
+    setTimeout(() => {
+        localStorage.setItem("search", event.target.value);
+        window.location.href = `${window.location.origin}/Final%20Project/movies.html` //this REROUTES to the same url of the page
+    }, 1000)
 }
 
 function alertPopUp() {
